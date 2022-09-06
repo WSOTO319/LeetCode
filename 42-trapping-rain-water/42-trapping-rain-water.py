bf66487411606532b74a0f12e0405a0ffdcae59a
WSOTO319 <wsoto001@plattsburgh.edu>
2022-09-06 09:43:03 -0400

    Time: 270 ms (17.65%), Space: 16 MB (81.43%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -2,19 +2,19 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         if not height: return 0
         
-        l, r = 0, len(height) - 1
-        leftMax, rightMax = height[l], height[r]
-        res = 0 
+        l, r = 0, len(height) - 1 # initialize left and right pointer variables
+        leftMax, rightMax = height[l], height[r] #initilaize the left and right max
+        res = 0 #initialize result variabe
         
-        while l < r:
-            if leftMax < rightMax:
-                l += 1
-                leftMax = max(leftMax, height[l])
-                res += leftMax - height[l]
+        while l < r: #while left pointer is less than right pointer 
+            if leftMax < rightMax: # if leftMax is less than rightMax 
+                l += 1 # move the left pointer up
+                leftMax = max(leftMax, height[l]) # leftMax will be the maximum between the current leftMax and the new height at the left pointer
+                res += leftMax - height[l] # add the difference between the leftMax and the current height to the result
                 
-            else:
-                r -= 1
-                rightMax = max(rightMax, height[r])
-                res += rightMax - height[r]
+            else: #if left pointer is not less than the right pointer
+                r -= 1 # move the right pointer down by one 
+                rightMax = max(rightMax, height[r]) #rightMax is the maximum between the current rightMax and the new height at the right pointer 
+                res += rightMax - height[r] # add the difference between the rightMax and the current height to the result 
                 
-        return res
+        return res #return the result
