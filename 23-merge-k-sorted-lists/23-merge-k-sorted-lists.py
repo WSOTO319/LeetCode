7583a887122d45c599372c6c716e6db4bf3156f9
WSOTO319 <wsoto001@plattsburgh.edu>
2022-09-21 22:55:07 -0400

    Time: 293 ms (17.37%), Space: 17.9 MB (45.44%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,19 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
-            return None
+        if not lists or len(lists) == 0: #if list is null or length is zero
+            return None # return none
 
-        while len(lists) > 1:
-            mergedLists = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
-            lists = mergedLists
-        return lists[0]
+        while len(lists) > 1:#while length of list is greater than 1 
+            mergedLists = []#initialize new list for merge
+            for i in range(0, len(lists), 2):# iterate through each list 
+                l1 = lists[i] # list 1 will be at index i 
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None #list 2 will be at i + 1 and if out of bounds then return an empty list 
+                mergedLists.append(self.mergeList(l1, l2)) #use function and append lists to mergeLists
+            lists = mergedLists#update lists variable to equal mergeLists  
+        return lists[0]# return lists[0]
 
-    def mergeList(self, l1, l2):
+    def mergeList(self, l1, l2): #helper function that actually merges lists
         dummy = ListNode()
         tail = dummy
 
