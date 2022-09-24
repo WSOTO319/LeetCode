424c7843809b1cf89c4fa141f5949b887339cb10
WSOTO319 <wsoto001@plattsburgh.edu>
2022-09-24 12:06:22 -0400

    Time: 68 ms (91.31%), Space: 15.2 MB (24.45%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,5 +5,5 @@ class Solution:
             # a + b = target -> a = target - b soo 
             a = target - b
             if a in seen:# if the number has been seen 
-                return [seen[a], i] # return the number's value(which is its index), and the current index of b 
+                return [seen[a], i] # return a's index, and the current index of b 
             seen[b] = i #else store the number in the seen dictionary with its value being its index
