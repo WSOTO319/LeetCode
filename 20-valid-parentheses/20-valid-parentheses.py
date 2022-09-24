12543eb8ab131f5db96d4a2cf737986d773ed2ee
WSOTO319 <wsoto001@plattsburgh.edu>
2022-09-24 13:05:23 -0400

    Time: 58 ms (34.45%), Space: 13.8 MB (98.14%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,10 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        d = {'(':')', '{':'}','[':']'}
-        stack = []
-        for i in s:
-            if i in d:  
-                stack.append(i)
-            elif len(stack) == 0 or d[stack.pop()] != i:  
-                return False
-        return len(stack) == 0 
+        d = {'(':')', '{':'}','[':']'} #map for the open and closing pairs
+        stack = [] #initialize a stack
+        for i in s: # go through each item in the string
+            if i in d:  #if the item is a key in the the map
+                stack.append(i) #append the item to the stack
+            elif len(stack) == 0 or d[stack.pop()] != i:  #elif the stack is empty OR the value being popped is not equal to the item/key's value 
+                return False # return False
+        return len(stack) == 0 #AFTER looping through the whole string return True if the stack is empty
